332d0556c4d1005e16eb45751f3dcf724527d6e4
Omar Mohamed <55915710+omarmuhamed@users.noreply.github.com>
2020-12-07 19:46:52 +0300

    Add files via upload

---
+++ Postfix to Infix Conversion using Stack/main.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct stack {
+    char data[100];
+    struct stack *next;
+} Stack;
+
+int isEmpty(Stack *stack) {
+    return stack != NULL;
+}
+
+void push(Stack **stack, char *data) {
+
+    Stack *temp;
+    temp = (Stack *) malloc(sizeof(Stack));
+    strcpy(temp->data, data);
+    temp->next = *stack;
+    *stack = temp;
+
+}
+
+char *pop(Stack **stack) {
+    Stack *temp;
+    if (isEmpty(*stack)) {
+        temp = *stack;
+        *stack = temp->next;
+        return temp->data;
+    }
+    return "";
+}
+
+
+int isOperator(char c) {
+    switch (c) {
+        case '*':
+        case '/':
+        case '+':
+        case '-':
+            return 1;
+        default:
+            return 0;
+    }
+
+}
+
+char *convert(char expression[]) {
+    char *a, *b;
+    char temp[100];
+    Stack *stack = (Stack *) malloc(sizeof(Stack));
+    stack->next = NULL;
+    stack->data[0] = '\0';
+
+    for (int i = 0; i < strlen(expression); i++) {
+        if (isOperator(expression[i])) {
+            a = pop(&stack);
+            b = pop(&stack);
+            temp[0] = '(';
+            temp[1] = '\0';
+            if (b[0]) {
+                strncat(temp, b, strlen(b));
+            }
+            strncat(temp, &expression[i], 1);
+            if (a[0]) {
+                strncat(temp, a, strlen(a));
+            }
+            strncat(temp, ")", 1);
+            strncat(temp, "\0", 1);
+
+            push(&stack, temp);
+        } else {
+            temp[0] = expression[i];
+            temp[1] = '\0';
+            push(&stack, temp);
+        }
+    }
+    return stack->data;
+}
+
+int main() {
+
+    char exp[100];
+    printf("Enter Postfix expression: ");
+    scanf("%s", exp);
+    printf("Infix: %s", convert(exp));
+    fflush(stdin);
+    getchar();
+
+}
